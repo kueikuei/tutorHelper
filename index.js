9d605d4914516f5b06dedfe2f6f271dcd593b05f
kueikuei <jasonchen@richitech.com.tw>
2019-08-18 12:02:28 +0800

    add res(200)

--- index.js
+++ index.js
@@ -6,15 +6,11 @@ admin.initializeApp({
     credential: admin.credential.cert(require("./sabot-dca8c-firebase-adminsdk-mqrmy-1c07d286ac.json")),
     databaseURL: "https://sabot-dca8c.firebaseio.com"
 });
-
 // 建立 db 連線
 var db = admin.database()
 var bot
-
-
 // [key, key, key]
 var keyList = []
-
 // 本地環境測試
 var localConfig = require('./localConfig.json')
 var localConfig
@@ -33,17 +29,14 @@ else {
         channelSecret: process.env.ChannelSecret
     })
 }
-
 bot.on('message', function (event) {
     console.log(event.message.text, event)
     // 沒有此 user id 就新增
     // if (keyList.indexOf(event.source.userId) === -1) {
     //     keyList.push(event.source.userId)
     // }
-
     if (event.message.type = 'text') {
         // console.log('event.message.text',typeof event.message.text)
-
         // 關鍵字回覆
         try {
             keyList.forEach(key => {
@@ -62,7 +55,6 @@ bot.on('message', function (event) {
         catch (e) {
             console.log(e) // 把例外物件傳給錯誤處理器
         }
-
         function rtnMsg(rtn) {
             event.reply(rtn).then(function (data) {
                 // 傳送訊息成功時，可在此寫程式碼 
@@ -72,31 +64,25 @@ bot.on('message', function (event) {
                 console.log('錯誤產生，錯誤碼：' + error);
             });
         }
-
     }
-
 });
-
-
 // 監聽 firebase 資料庫
 // 每一次資料庫有更新這邊都能偵測
 // 更新清單資料以利使用
 db.ref('tutor').on('value', function (snapshot) {
     var dataKeys = Object.keys(snapshot.val())
     keyList = dataKeys
-
 })
-
-
 // 開啟 port
 const app = express();
 const linebotParser = bot.parser();
-app.post('/', linebotParser);
-
+// app.post('/', linebotParser);
+app.post('/',function (req, res, linebotParser){
+    res.status(200)
+})
 // Bot所監聽的webhook路徑與port
 // for local test
-
-var server = app.listen(process.env.PORT || 8080, function () {
+var server = app.listen(process.env.PORT || 8080, function (req,res) {
     var port = server.address().port;
     console.log('目前的port是', port);
 });
